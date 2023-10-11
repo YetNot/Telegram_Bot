c3de9d3d5e3d4d805c5baee78c991c9186601c9b
Kirill Glinskikh <glinskix24@bk.ru>
2023-10-11 15:39:52 +0300

    help && list

---
+++ cmd/bot/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/YetNot/bot/internal/service/product"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load()
+	token := os.Getenv("TOKEN")
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.UpdateConfig{
+		Timeout: 60,
+	}
+
+	updates := bot.GetUpdatesChan(u)
+
+	productService := product.NewService()
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+
+			switch update.Message.Command() {
+			case "help":
+				helpCommand(bot, update.Message)
+			case "list":
+				listCommand(bot, update.Message, productService)
+			default:
+				defaultBehavior(bot, update.Message)
+			}
+		}
+	}
+}
+
+func helpCommand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID,
+		"/help - help\n"+
+			"/list - list entities",
+	)
+	bot.Send(msg)
+}
+
+func listCommand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message, productService *product.Service) {
+	outPutMsgText := "Here all the products: \n\n"
+
+	products := productService.List()
+	for _, p := range products {
+		outPutMsgText += p.Title
+		outPutMsgText += "\n"
+	}
+
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, outPutMsgText)
+	bot.Send(msg)
+}
+
+func defaultBehavior(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
+	log.Printf("[%s] %s", inputMessage.From.UserName, inputMessage.Text)
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "Твоё сообщение: "+inputMessage.Text)
+	//msg.ReplyToMessageID = update.Message.MessageID
+	bot.Send(msg)
+}
